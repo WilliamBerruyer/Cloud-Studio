18ed4ae0b0a93165187dc798bcae0b3ea6a68462
William Berruyer <william.berruyer3@gmail.com>
2024-11-04 16:00:40 +0100

    added ssl

--- model/DAO.inscription.php
+++ model/DAO.inscription.php
@@ -7,6 +7,7 @@ try
     $connection = new \PDO($dsn, 'ouyppnrxzt', 'Pimaille05');
     $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     $connection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+    $connection->setAttribute(PDO::MYSQL_ATTR_SSL_CA => 'SSL/DigiCertGlobalRootCA.crt.pem');
 
 }
 catch(Exception $e)
