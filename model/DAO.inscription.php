727599414f2f98768d62878e1f83d915177b2c8e
William Berruyer <william.berruyer3@gmail.com>
2024-11-04 16:27:10 +0100

    sssssssss

--- model/DAO.inscription.php
+++ model/DAO.inscription.php
@@ -3,7 +3,7 @@ require_once("Utilisateur.php");
 
 try
 {
-    $dsn = 'mysql:dbname=clstudio-database;host=cloudstudio.mysql.database.azure.com;port=3306;charset=utf8';
+    $dsn = 'mysql:dbname=clstudio-database;host=clstudio-server.mysql.database.azure.com;port=3306;charset=utf8';
     $connection = new \PDO($dsn, 'ouyppnrxzt', 'Pimaille05');
     $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     $connection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
